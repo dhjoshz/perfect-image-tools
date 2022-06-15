03a69b637108ecf5159aea58f7c2245cd8e3736e
oliver zulett <joshua.zulett@dharbor.com>
2022-06-15 21:33:42 +0000

    feat: swagger implementation

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { APPLICATION_CONFIG } from '../config/constants';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const properties = configService.get(`${APPLICATION_CONFIG}`);
-  const port = properties.port;
-  await app.listen(port);
+  const API_PORT = properties.port;
+  const config = new DocumentBuilder()
+    .setTitle('Image Tools')
+    .setDescription('Image Tools service')
+    .setVersion('1.0')
+    .addTag('health-check')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(API_PORT);
 }
 bootstrap();
--- src/resources/health-check/health-check.controller.ts
+++ src/resources/health-check/health-check.controller.ts
@@ -1,7 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('health-check')
 @Controller('health-check')
 export class HealthCheckController {
+  @ApiOkResponse({
+    description: 'Get Service status',
+  })
   @Get()
   checkHealth() {
     return `I'm Alive`;
